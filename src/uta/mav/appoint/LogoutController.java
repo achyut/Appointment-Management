a0f6d0f6e258361c6dd74668e6a0fcf9d10133b1
achyut <achyut.pdl@gmail.com>
2015-11-02 18:42:16 -0600

    source files added

---
+++ src/uta/mav/appoint/LogoutController.java
@@ -0,0 +1,29 @@
+package uta.mav.appoint;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	HttpSession session;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		session = request.getSession();
+		session.invalidate();
+		session = request.getSession();
+		session.setAttribute("message", "Logout sucessfully done.");
+		response.sendRedirect("index");
+	}
+
+}
